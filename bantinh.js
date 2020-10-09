c7cb36382ee993ee6eb9845abbab6bd8c2006758
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-09 22:13:27 +0900

    sua cac function

--- bantinh.js
+++ bantinh.js
@@ -2,21 +2,21 @@ function tong() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let tong = sothu1 + sothu2;
-    let hieu = sothu1 - sothu2;
-    let tich = sothu1 * sothu2;
-    let thuong = sothu1 / sothu2;
-    phatngon = "Tổng 2 số là" + tổng
-    document.getElementById("phatngon").innerText = tong
+    phatngon = "Tổng 2 số là" + tong
+    document.getElementById("phatngon").innerText = phatngon
 }
 function hieu() {
+    let hieu = sothu1 - sothu2;
     phatngon = "Hiệu 2 số là:" + hieu
-    document.getElementById("phatngon").innerText = hieu
+    document.getElementById("phatngon").innerText = phatngon
 }
 function tich() {
+    let tich = sothu1 * sothu2;
     phatngon = "Tích 2 số là:" + tich
-    document.getElementById("phatngon").innerText = tich
+    document.getElementById("phatngon").innerText = phatngon
 }
 function thuong() {
+    let thuong = sothu1 / sothu2;
     phatngon = "Thương 2 số là:" + thuong
-    document.getElementById("phatngon").innerText = thuong
+    document.getElementById("phatngon").innerText = phatngon
 }
