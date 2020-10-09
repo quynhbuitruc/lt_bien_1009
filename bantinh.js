8b39c81878ad6323188ae91522fa7ab59c6d27c0
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-09 21:49:46 +0900

    dat bien tong

--- bantinh.js
+++ bantinh.js
@@ -1,1 +1,3 @@
-let sothu1 = Number(document.getElementById(so1))
+let sothu1 = Number(document.getElementById("so1").value)
+let sothu2 = Number(document.getElementById("so2").value)
+let tong = sothu1 + sothu2;
