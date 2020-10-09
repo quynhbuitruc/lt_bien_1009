b3469f43adc22aa3d7a1dd103b6e76ff8bd984cb
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-09 21:53:01 +0900

    dat cac bien tong hieu tich thuong

--- bantinh.js
+++ bantinh.js
@@ -1,3 +1,10 @@
 let sothu1 = Number(document.getElementById("so1").value)
 let sothu2 = Number(document.getElementById("so2").value)
 let tong = sothu1 + sothu2;
+let hieu = sothu1 - sothu2;
+let tich = sothu1 * sothu2;
+let thuong = sothu1 / sothu2;
+document.getElementById("phatngon").innerHTML = tong
+document.getElementById("phatngon").innerHTML = hieu
+document.getElementById("phatngon").innerHTML = tich
+document.getElementById("phatngon").innerHTML = thuong
