f74afe007f04cbd9c00d791f5cf53b955e0732fa
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-09 23:47:39 +0900

    focus2

--- bantinh.js
+++ bantinh.js
@@ -2,21 +2,41 @@ function tong() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let tong = sothu1 + sothu2;
-    phatngon = "Tổng 2 số là" + tong
+    hienthi = document.getElementById("so1").value + " + " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi").innerText = hienthi
+    phatngon = tong
     document.getElementById("phatngon").innerText = phatngon
 }
 function hieu() {
+    let sothu1 = Number(document.getElementById("so1").value)
+    let sothu2 = Number(document.getElementById("so2").value)
     let hieu = sothu1 - sothu2;
-    phatngon = "Hiệu 2 số là:" + hieu
+    hienthi = document.getElementById("so1").value + " - " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi").innerText = hienthi
+    phatngon = hieu
     document.getElementById("phatngon").innerText = phatngon
 }
 function tich() {
+    let sothu1 = Number(document.getElementById("so1").value)
+    let sothu2 = Number(document.getElementById("so2").value)
     let tich = sothu1 * sothu2;
-    phatngon = "Tích 2 số là:" + tich
+    hienthi = document.getElementById("so1").value + " * " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi").innerText = hienthi
+    phatngon = tich
     document.getElementById("phatngon").innerText = phatngon
 }
 function thuong() {
+    let sothu1 = Number(document.getElementById("so1").value)
+    let sothu2 = Number(document.getElementById("so2").value)
     let thuong = sothu1 / sothu2;
-    phatngon = "Thương 2 số là:" + thuong
+    hienthi = document.getElementById("so1").value + " : " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi").innerText = hienthi
+    phatngon = thuong
     document.getElementById("phatngon").innerText = phatngon
+    
+}
+
+function use() {
+    document.getElementById("so1").value = phatngon
+    document.getElementById("so2").focus();
 }
