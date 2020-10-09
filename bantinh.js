ea83df784c610af46380d967ea5f30e24f688e26
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-09 22:08:22 +0900

    dat bien phatngon

--- bantinh.js
+++ bantinh.js
@@ -5,8 +5,12 @@ function tong() {
     let hieu = sothu1 - sothu2;
     let tich = sothu1 * sothu2;
     let thuong = sothu1 / sothu2;
+    phatngon = "Tổng 2 số là" + tổng
     document.getElementById("phatngon").innerHTML = tong
+    phatngon = "Hiệu 2 số là:" + hieu
     document.getElementById("phatngon").innerHTML = hieu
+    phatngon = "Tích 2 số là:" + tich
     document.getElementById("phatngon").innerHTML = tich
+    phatngon = "Thương 2 số là:" + thuong
     document.getElementById("phatngon").innerHTML = thuong
 }
