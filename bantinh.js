12894173d4ed6c7450aa9398a0b284ddcb86b203
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-11 16:36:55 +0900

    sửa function equal

--- bantinh.js
+++ bantinh.js
@@ -2,41 +2,43 @@ function tong() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let tong = sothu1 + sothu2;
-    hienthi = document.getElementById("so1").value + " + " + document.getElementById("so2").value + "  = "
-    document.getElementById("hienthi").innerText = hienthi
-    phatngon = tong
-    document.getElementById("phatngon").innerText = phatngon
+    hienthi1 = document.getElementById("so1").value + " + " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi1").innerText = hienthi1
+    phatngon1 = tong
+    document.getElementById("phatngon1").innerText = phatngon1
 }
 function hieu() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let hieu = sothu1 - sothu2;
-    hienthi = document.getElementById("so1").value + " - " + document.getElementById("so2").value + "  = "
-    document.getElementById("hienthi").innerText = hienthi
-    phatngon = hieu
-    document.getElementById("phatngon").innerText = phatngon
+    hienthi1 = document.getElementById("so1").value + " - " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi1").innerText = hienthi1
+    phatngon1 = hieu
+    document.getElementById("phatngon1").innerText = phatngon1
 }
 function tich() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let tich = sothu1 * sothu2;
-    hienthi = document.getElementById("so1").value + " * " + document.getElementById("so2").value + "  = "
-    document.getElementById("hienthi").innerText = hienthi
-    phatngon = tich
-    document.getElementById("phatngon").innerText = phatngon
+    hienthi1 = document.getElementById("so1").value + " * " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi1").innerText = hienthi1
+    phatngon1 = tich
+    document.getElementById("phatngon1").innerText = phatngon1
 }
 function thuong() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let thuong = sothu1 / sothu2;
-    hienthi = document.getElementById("so1").value + " : " + document.getElementById("so2").value + "  = "
-    document.getElementById("hienthi").innerText = hienthi
-    phatngon = thuong
-    document.getElementById("phatngon").innerText = phatngon
-    
+    hienthi1 = document.getElementById("so1").value + " : " + document.getElementById("so2").value + "  = "
+    document.getElementById("hienthi1").innerText = hienthi1
+    phatngon1 = thuong
+    document.getElementById("phatngon1").innerText = phatngon1
+
 }
 
 function euqal() {
+    document.getElementById("hienth2").innerHTML = hienthi1
+    document.getElementById("hienthi2").innerText = phatngon1
     document.getElementById("so1").value = phatngon
     document.getElementById("so2").focus();
     document.getElementById("so2").value = ""
