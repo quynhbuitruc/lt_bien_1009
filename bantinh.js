3627352355ea21db5d7bb02959d596f88510f9a6
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 21:18:13 +0900

    thêm let trước các dòng phatngon1

--- bantinh.js
+++ bantinh.js
@@ -2,36 +2,36 @@ function tong() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let tong = sothu1 + sothu2;
-    hienthi1 = document.getElementById("so1").value + " + " + document.getElementById("so2").value + "  = "
+    let hienthi1 = document.getElementById("so1").value + " + " + document.getElementById("so2").value + "  = "
     document.getElementById("hienthi1").innerText = hienthi1
-    phatngon1 = tong
+    let phatngon1 = tong
     document.getElementById("phatngon1").innerText = phatngon1
 }
 function hieu() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let hieu = sothu1 - sothu2;
-    hienthi1 = document.getElementById("so1").value + " - " + document.getElementById("so2").value + "  = "
+    let hienthi1 = document.getElementById("so1").value + " - " + document.getElementById("so2").value + "  = "
     document.getElementById("hienthi1").innerText = hienthi1
-    phatngon1 = hieu
+    let phatngon1 = hieu
     document.getElementById("phatngon1").innerText = phatngon1
 }
 function tich() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let tich = sothu1 * sothu2;
-    hienthi1 = document.getElementById("so1").value + " * " + document.getElementById("so2").value + "  = "
+    let hienthi1 = document.getElementById("so1").value + " * " + document.getElementById("so2").value + "  = "
     document.getElementById("hienthi1").innerText = hienthi1
-    phatngon1 = tich
+    let phatngon1 = tich
     document.getElementById("phatngon1").innerText = phatngon1
 }
 function thuong() {
     let sothu1 = Number(document.getElementById("so1").value)
     let sothu2 = Number(document.getElementById("so2").value)
     let thuong = sothu1 / sothu2;
-    hienthi1 = document.getElementById("so1").value + " : " + document.getElementById("so2").value + "  = "
+    let hienthi1 = document.getElementById("so1").value + " : " + document.getElementById("so2").value + "  = "
     document.getElementById("hienthi1").innerText = hienthi1
-    phatngon1 = thuong
+    let phatngon1 = thuong
     document.getElementById("phatngon1").innerText = phatngon1
 
 }
